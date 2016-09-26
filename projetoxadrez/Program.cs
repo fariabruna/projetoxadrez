73dddd2c372fea2e46c04a973bd58aa437ed952b
Bruna Faria <fariabruna71@gmail.com>
2016-09-26 09:22:51 -0300

    Primeiro commit

---
+++ projetoxadrez/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projetoxadrez
+{
+    class Program
+    {
+        static void Main(string[] args){
+
+            Console.Write("Digite uma valor inteiro: ");
+
+            int x = int.Parse(Console.ReadLine());
+
+            int y = 2 * x;
+            Console.WriteLine("DOBRO = " + y);
+            Console.ReadLine();
+        }
+    }
+}
